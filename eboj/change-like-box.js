a72935758bccab9d834d8da18a35c49534daac70
fienestar <fienestar@gmail.com>
2024-03-19 10:10:47 +0900

    fix: #3

--- eboj/change-like-box.js
+++ eboj/change-like-box.js
@@ -1,25 +1,33 @@
 function change_like_box()
 {
     Array.from(document.getElementsByClassName('like-box')).forEach(v => {
+        const like_box = v.cloneNode(true);
+        v.style.opacity = 0
+        v.style.maxWidth = 0
+        like_box.className = 'cloned-like-box'
+        like_box.style.display = ''
+        v.parentElement.insertBefore(like_box, v)
+
         const button = v.nextElementSibling.children[0]
         if (button.style.display == 'none') return;
 
-        v.style.display = ''
         button.style.display = 'none'
 
         const update = () => {
             let enabled = button.innerText.includes('취소')
 
-            if (enabled) v.style.color = '#0076C0'
-            else v.style.color = ''
+            if (enabled) like_box.style.color = '#0076C0'
+            else like_box.style.color = ''
 
-            if (v.children[1].innerText == '0')
-                v.children[1].style.display = 'none'
+            like_box.children[1].innerText = v.children[1].innerText
+
+            if (like_box.children[1].innerText == '0')
+            like_box.children[1].style.display = 'none'
             else
-                v.children[1].style.display = ''
+            like_box.children[1].style.display = ''
         }
 
-        v.addEventListener('click', () => {
+        like_box.addEventListener('click', () => {
             button.click()
         })
 
